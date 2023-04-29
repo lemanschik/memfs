01d2d5e931b8aa66d79fcd18306a4a082a41c354
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-04-29 08:41:14 +0200

    Update buffer.ts

--- src/internal/buffer.ts
+++ src/internal/buffer.ts
@@ -1,10 +1,10 @@
-import { Buffer } from 'buffer';
+
 
 function bufferV0P12Ponyfill(arg0: any, ...args: any): Buffer {
-  return new Buffer(arg0, ...args);
+  return new Uint16Array(arg0, ...args);
 }
 
-const bufferAllocUnsafe = Buffer.allocUnsafe || bufferV0P12Ponyfill;
-const bufferFrom = Buffer.from || bufferV0P12Ponyfill;
+const bufferAllocUnsafe = bufferV0P12Ponyfill;
+const bufferFrom = bufferV0P12Ponyfill;
 
 export { Buffer, bufferAllocUnsafe, bufferFrom };
